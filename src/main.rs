7ae975d3fd287e4a9add6550410ba9b086501a4f
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2020-04-19 20:31:45 +0900

    Modify to be able to set the gamma in `peaks` subcommand

--- src/main.rs
+++ src/main.rs
@@ -27,23 +27,33 @@ enum Opt {
         /// The end value of the parameter λ
         #[structopt(long,short)]
         end: f64,
+        /// The value of the parameter γ
+        #[structopt(long,short,default_value="1.0")]
+        gamma: f64,
         /// The step size of incrementing the parameter λ
         #[structopt(short="d")]
         step: f64,
         /// The initial value of the infected population
         #[structopt(long,short)]
         infected: f64,
+        /// The total population
+        #[structopt(short="n",default_value="1.0")]
+        population: f64,
     }
 }
 
-fn simulate(l: f64, g: f64, i: f64, n: f64) {
+fn calculate_step_size(n: f64) -> (f64, usize) {
     let e = n.log10().floor();
     let s = n / 10.0_f64.powf(e);
     let s = s.floor();
     let dt = 0.01 / (s * 10.0_f64.powf(e));
     let print_interval = (s * 10.0_f64.powf(e+1.0)) as usize;
-    eprintln!("dt = {}", dt);
+    (dt, print_interval)
+}
 
+fn simulate(l: f64, g: f64, i: f64, n: f64) {
+    let (dt, print_interval) = calculate_step_size(n);
+    eprintln!("dt = {}", dt);
     let eom = sir_peaks::SirModel { lambda: l, gamma: g };
     let mut teo = eom::explicit::RK4::new(eom, dt);
     let ts = eom::adaptor::time_series(arr1(&[n-i, i, 0.0]), &mut teo);
@@ -52,7 +62,6 @@ fn simulate(l: f64, g: f64, i: f64, n: f64) {
         if counter == 0 {
             println!("{} {} {}",v[0], v[1], v[2]);
         }
-
         if counter == print_interval-1 {
             counter = 0;
         } else {
@@ -61,13 +70,13 @@ fn simulate(l: f64, g: f64, i: f64, n: f64) {
     }
 }
 
-fn peaks(i: f64, start: f64, end: f64, step: f64) {
-    let dt = 0.01;
-    let initial_condition = arr1(&[1.0-i, i, 0.0]);
+fn peaks(i: f64, start: f64, end: f64, step: f64, g: f64, n: f64) {
+    let (dt, _) = calculate_step_size(n);
+    let initial_condition = arr1(&[n-i, i, 0.0]);
 
     let mut l = start;
     while l < end+step {
-        let eom = sir_peaks::SirModel { lambda: l, gamma: 1.0 };
+        let eom = sir_peaks::SirModel { lambda: l, gamma: g };
         let mut teo = eom::explicit::RK4::new(eom, dt);
         let ts = eom::adaptor::time_series(initial_condition.clone(), &mut teo);
         let peak = ts.take_while(|v| v[1] > i/10.0)
@@ -94,16 +103,16 @@ fn main() {
             eprintln!("{}", l*n/g);
             simulate(l, g, i, n);
         },
-        Opt::Peaks { start, end, step, infected } => {
+        Opt::Peaks { start, end, step, gamma, infected, population } => {
             eprintln!("# peaks");
             eprintln!("## parameters");
             eprintln!("- l : {}:{}", start, end);
-            eprintln!("- g : {}", 1.0);
+            eprintln!("- g : {}", gamma);
             eprintln!("## initial values");
-            eprintln!("- S : {}", 1.0-infected);
+            eprintln!("- S : {}", population-infected);
             eprintln!("- I : {}", infected);
             eprintln!("- R : {}", 0.0);
-            peaks(infected, start, end, step);
+            peaks(infected, start, end, step, gamma, population);
         }
     }
 }
